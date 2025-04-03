0ed7857a221e2fc255f4655d818530193a59a3a5
Zohaibkhan2610 <zohaibkhan2610765@gmail.com>
2025-04-03 09:49:57 +0500

    v0 init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { MdGroup } from 'react-icons/md'
+import "@fontsource/poppins";
+import "@fontsource/poppins/400.css"; 
+import "@fontsource/poppins/400-italic.css";
+
+function App() {
+
+  return (
+   <>
+   <h1 className="text-6xl">Ready</h1>
+   <MdGroup className='text-amber-300 text-6xl'/>
+   </>
+  )
+}
+
+export default App
